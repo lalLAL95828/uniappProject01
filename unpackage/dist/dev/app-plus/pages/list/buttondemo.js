cffbb10b360819247a8be97f5e9d7e65057cea5b
lal <1445668063@qq.com>
2019-10-20 16:51:31 +0800

    删除unpackage

--- unpackage/dist/dev/app-plus/pages/list/buttondemo.js
+++
@@ -1,2 +0,0 @@
-setCssToHead(["@charset \x22UTF-8\x22;\n.",[1],"style01 { color: #999; }\n",],undefined,{path:"./pages/list/buttondemo.wxss"})();
-document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/list/buttondemo.wxml') } }));
